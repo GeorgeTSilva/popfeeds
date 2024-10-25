ba0cce1ccaec512817447b5b9b2fd78d3d84aa27
George T. Silva <georges083@gmail.com>
2024-10-24 21:20:36 -0400

    Update main.py
    Updated port for Render to 10000 from 8080

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ scheduler.start()
 #app.run(host='0.0.0.0', port=8080)
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=10000)
